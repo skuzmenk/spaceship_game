0f4fcd3c4c6fb33e8b762b73bcc688675011cd3e
skuzmenk <kuzmenko.oleksandr1123@vu.cdu.edu.ua>
2025-05-12 08:41:10 +0300

    added main components, changed mainwindow settings, added methods to load sound and image and added start screen

--- spaceship_game/MainWindow.xaml.cs
+++ spaceship_game/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
-﻿using System.Text;
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Text.Json;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace spaceship_game
@@ -16,9 +17,79 @@ namespace spaceship_game
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isGameOver = false;
+        private int lives = 3;
+        private int count = 1;
+        private int health = 0;
+        private int score = 0;
+        private int speed = 60;
+        private int highScore = 0;
+        private string highScorePath = "highscore.json";
+
+        private Random random = new();
+        private SoundPlayer shoot = LoadSound("Resources/zvuk-vyistrela-iz-lazera-23704.wav");
+        private SoundPlayer player = LoadSound("Resources/tyajelyiy-raskatistyiy-zvuk-vzryiva.wav");
+        private SoundPlayer gameOver = LoadSound("Resources/uroven-ne-proyden.wav");
+        private SoundPlayer bossRound = LoadSound("Resources/jutkii-smeh-odinochnyii-mujskoi-korotkii.wav");
+
+        private Image ship;
+        private Image heart1;
+        private Image heart2;
+        private Image heart3;
+        private Canvas gameCanvas;
+        private Label scoreLabel;
         public MainWindow()
         {
             InitializeComponent();
+            this.WindowState = WindowState.Maximized;  
+            this.WindowStyle = WindowStyle.None;
+            this.KeyDown += Window_KeyDown;
+            this.Topmost = true;  
+            gameCanvas = new Canvas();
+            this.Content = gameCanvas;
+            ShowStartScreen();     
+        }
+        private async void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+        private void ShowStartScreen()
+        {
+            gameCanvas.Children.Clear();
+
+            Image background = new()
+            {
+                Source = LoadImage("Resources/CosmoBattle.jpg"),
+
+                Stretch = Stretch.Fill
+            };
+            this.SizeChanged += (s, e) =>
+            {
+                background.Width = this.Width;
+                background.Height = this.Height;
+            };
+            gameCanvas.Children.Add(background);
+
+            Label startLabel = new()
+            {
+                Content = "Press Space to start",
+                Foreground = Brushes.Yellow,
+                FontSize = 20,
+                FontWeight = FontWeights.Bold
+            };
+            Canvas.SetLeft(startLabel, 650);
+            Canvas.SetTop(startLabel, 750);
+            gameCanvas.Children.Add(startLabel);
+
+        }
+        private static SoundPlayer LoadSound(string path)
+        {
+            return new SoundPlayer(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path));
+        }
+
+        public static BitmapImage LoadImage(string path)
+        {
+            return new BitmapImage(new Uri(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, path)));
         }
     }
 }
